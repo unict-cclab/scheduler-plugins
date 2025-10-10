6c9a43d96225b1ee9bb933621da14cbe5e51c293
DamianoSamperi <damihearts@hotmail.it>
2025-10-10 12:14:00 +0200

    Add ArchitectureAware postbind plugin

---
+++ pkg/sophos/ArchitectureAware/architectureaware.go
@@ -0,0 +1,86 @@
+package architectureaware
+
+import (
+	"context"
+	"fmt"
+        metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog/v2"
+	"k8s.io/kubernetes/pkg/scheduler/framework"
+)
+
+const (
+	Name = "ArchitectureAware"
+)
+
+type ArchitectureAware struct {
+	handle framework.Handle
+}
+
+var _ = framework.PreBindPlugin(&ArchitectureAware{})
+
+
+func (pl *ArchitectureAware) Name() string {
+	return Name
+}
+func (pl *ArchitectureAware) PreBind(ctx context.Context,  _ *framework.CycleState, pod *v1.Pod, nodeName string) *framework.Status{
+    klog.Infof("[ArchitectureAware] PreBind: Pod %s/%s to node %s", pod.Namespace, pod.Name, nodeName)
+
+	client := pl.handle.ClientSet()
+
+	node, err := client.CoreV1().Nodes().Get(ctx, nodeName, metav1.GetOptions{})
+	if err != nil {
+		msg := fmt.Sprintf("Failed to find node %s: %v", nodeName, err)
+		klog.Error(msg)
+		return framework.NewStatus(framework.Error, msg)
+	}
+    //label to choose
+	deviceType := node.Labels["nvidia.com/device-plugin.config"]
+	klog.Infof("[ArchitectureAware] Node %s have label nvidia.com/device-plugin.config=%s", nodeName, deviceType)
+
+	var newImage string
+	switch deviceType {
+	case "orin":
+		newImage = "192.168.1.252:480/jetson/multicomponent_service:r36"
+	case "nano":
+		newImage = "192.168.1.252:480/jetson/multicomponent_service:latest"
+	default:
+		klog.Warningf("[ArchitectureAware] Node %s without valid label (%s), Image not modified", nodeName, deviceType)
+		return framework.NewStatus(framework.Success, "")
+	}
+
+	podCopy := pod.DeepCopy()
+	updated := false
+	for i := range podCopy.Spec.Containers {
+		if podCopy.Spec.Containers[i].Name == "nn" {
+			oldImage := podCopy.Spec.Containers[i].Image
+			podCopy.Spec.Containers[i].Image = newImage
+			klog.Infof("[ArchitectureAware] Change container 'nn' image from %s → %s", oldImage, newImage)
+			updated = true
+			break
+		}
+	}
+
+	if !updated {
+		klog.Warningf("[ArchitectureAware] No container 'nn' find in the Pod %s/%s", pod.Namespace, pod.Name)
+		return framework.NewStatus(framework.Success, "")
+	}
+
+	_, err = client.CoreV1().Pods(pod.Namespace).Update(ctx, podCopy, metav1.UpdateOptions{})
+	if err != nil {
+		msg := fmt.Sprintf("Failed to update image on Pod %s/%s: %v", pod.Namespace, pod.Name, err)
+		klog.Error(msg)
+		return framework.NewStatus(framework.Error, msg)
+	}
+
+	klog.Infof("[ArchitectureAware] Pod %s/%s update with image %s on container 'nn'", pod.Namespace, pod.Name, newImage)
+	return framework.NewStatus(framework.Success, "")
+}
+
+func New(_ context.Context, _ runtime.Object, handle framework.Handle) (framework.Plugin, error) {
+	pl := &ArchitectureAware{
+		handle: handle,
+	}
+	return pl, nil
+}

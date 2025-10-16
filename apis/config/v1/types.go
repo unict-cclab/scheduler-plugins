617b8b414fe2eb7c6f51c95ecefe77654d9a9b55
DamianoSamperi <damihearts@hotmail.it>
2025-10-16 19:06:50 +0200

    update

--- apis/config/v1/types.go
+++ apis/config/v1/types.go
@@ -24,6 +24,16 @@ import (
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
+type ArchitectureAwareArgs struct {
+    metav1.TypeMeta `json:",inline"`
+
+    OrinTag string `json:"orinTag,omitempty"`
+    NanoTag string `json:"nanoTag,omitempty"`
+}
+
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
 // CoschedulingArgs defines the scheduling parameters for Coscheduling plugin.
 type CoschedulingArgs struct {
 	metav1.TypeMeta `json:",inline"`

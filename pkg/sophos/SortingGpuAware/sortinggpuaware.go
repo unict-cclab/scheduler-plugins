d21ac60790581d7d39273ec92c8b4ba95066da57
DamianoSamperi <damihearts@hotmail.it>
2025-12-05 13:05:43 +0100

    correction

--- pkg/sophos/SortingGpuAware/sortinggpuaware.go
+++ pkg/sophos/SortingGpuAware/sortinggpuaware.go
@@ -2,16 +2,11 @@ package sortinggpuaware
 
 import (
 	"context"
-	"math"
-	"strconv"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
-	"sigs.k8s.io/scheduler-plugins/apis/config"
-
-	"runtime"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 const SortingName = "SortingGpuAware"
